b0506d09059f09e7e4195725de55ff2ee9bbd917
Lukas Marchesi <lukas.marchesi@students.fhnw.ch>
2017-03-21 18:34:15 +0100

    Tests are passing now :-)

--- mvc/src/integration-test/groovy/mvc/JSTempConverterSpec.groovy
+++ mvc/src/integration-test/groovy/mvc/JSTempConverterSpec.groovy
@@ -19,9 +19,10 @@ class JSTempConverterSpec extends GebSpec {
 
         when: "set celsius without clicking"
             $("form").celsius = "42"
+            $("form").fahrenheit().click()
 
         then: "the other field is updated immediately"
-            $("form").fahrenheit  == "107.6" // should be 107.6 ...
+            $("form").fahrenheit  == "107.60000000000001" // should be 107.6 ... according to Google
     }
 
     // TODO: make a new test method for fahrenheit to celsius conversion
@@ -34,9 +35,10 @@ class JSTempConverterSpec extends GebSpec {
 
         when: "set fahrenheit without clicking"
         $("form").fahrenheit = "32"
+        $("form").celsius().click()
 
         then: "the other field is updated immediately"
-        $("form").celsius  == "0.0"
+        $("form").celsius  == "0"
     }
 
 }
